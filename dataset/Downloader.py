b2ce633d0e1139b99ef9eec2bd72d255d601a45c
denisgasparollo <gasparollodenis@gmail.com>
2022-04-13 09:46:19 +0200

    misc fixing

--- dataset/Downloader.py
+++ dataset/Downloader.py
@@ -15,7 +15,7 @@ class Downloader(Thread):
         self.LOG_NOTEXIST = "\tFile non exist"
         self.num_worker = num_worker
 
-    def mta_download(self, url, dest, log_file):
+    def __mta_download(self, url, dest, log_file):
         if not dest[-1] == '/':
             dest += '/'
 
@@ -25,10 +25,11 @@ class Downloader(Thread):
         src = req.get(url).text
         soup = BeautifulSoup(src, 'lxml')
 
-        name = soup.find('a', class_='menu_link').text
+        name = soup.find('a', class_='menu_link')['href']
 
         # download pcapFile
-        pcap_url = url[0: len(url) - 10] + name
+        url_splited = url.split('/')
+        pcap_url = '/'.join(url_splited[: len(url_splited) - 1]) + f'/{name}'
         res = req.get(pcap_url)
 
         print(pcap_url)
@@ -42,13 +43,13 @@ class Downloader(Thread):
             print(self.LOG_FAIL)
             log_file.write(f'{self.LOG_FAIL}\n')
 
-    def build_sample_attr(self, sample_name, scenario):
+    def __build_sample_attr(self, sample_name, scenario):
         if scenario == "original":
             return f"trazasOriginal/5GBdirectory/{sample_name}"
         else:
             return f"trazasNAT/{sample_name}"
 
-    def ha_download(self, sample_name, scenario, dest, log_file):
+    def __ha_download(self, sample_name, scenario, dest, log_file):
         if not dest[-1] == '/':
             dest += '/'
 
@@ -57,7 +58,7 @@ class Downloader(Thread):
 
         url1 = "http://dataset.tlm.unavarra.es/ransomware/php/descargar.php?"  # usato per reperire il nome del file
         url2 = "http://dataset.tlm.unavarra.es/ransomware/downloads"  # usato per scaricare il file
-        sample_value = self.build_sample_attr(sample_name, scenario)
+        sample_value = self.__build_sample_attr(sample_name, scenario)
 
         payload = {'tokenDescarga': '',
                    'descargarTodo': '0',
@@ -97,32 +98,37 @@ class Downloader(Thread):
 
             if "malware-traffic-analysis.net" in sample["Link"]:
                 with open('download.log', 'a') as log:
-                    self.mta_download(sample["Link"], 'raw/pcap/test/', log)
+                    self.__mta_download(sample["Link"], 'raw/pcap/test/', log)
             elif "hybrid-analysis.com" in sample["Link"]:
                 with open('download.log', 'a') as log:
-                    self.ha_download(sample["SamplePcap"], sample["Scenario"], 'raw/pcap/test/', log)
+                    self.__ha_download(sample["SamplePcap"], sample["Scenario"], 'raw/pcap/test/', log)
 
             #request = urllib.request.Request(content)
             #response = urllib.request.urlopen(request)
             #self.results.append(response.read())
             self.queue.task_done()
 
+#------------------------------------------------------------
 
 elems = [{
-            "SamplePcap": "samplesPcap_compress/Cryxox_27122018.pcap.gz",
-            "Hash": "7cc1402c8d3b33f91da147597ed4dd47",
-            "Link": "https://www.hybrid-analysis.com/sample/e75ee5dcc9921d016f9d33989cfebe97db006354699f7d005d82801a3daa8920?environmentId=100",
-            "Scenario": "original"
+            "SamplePcap": "samplesPcap_compress/cerber_03102016.pcap.gz",
+            "Hash": "b17ff50e5abca3e850a433af42107314",
+            "Link": "https://www.malware-traffic-analysis.net/2016/10/03/index2.html",
+            "Scenario": "NAT"
         },
         {
             "SamplePcap": "samplesPcap_compress/Crysis_17092018.pcap.gz",
             "Hash": "e9d396504c415eb746396ec310eacd1f",
             "Link": "https://www.hybrid-analysis.com/sample/05fd72a70e0604da8e888b1ecbab2e3ca77fe079a2abe06157769e30e22dedcd?environmentId=120",
             "Scenario": "NAT"
-        }]
-
-#------------------------------------------------------------
-
+        },
+        {
+            "SamplePcap": "samplesPcap_compress/Spora_17052017.pcap.gz",
+            "Hash": "685c6834f8f81948822762cd13a48604",
+            "Link": "https://www.malware-traffic-analysis.net/2017/05/17/index.html",
+            "Scenario": "NAT"
+        }
+        ]
 samples = queue.Queue()
 NO_WORKER = 2
 
