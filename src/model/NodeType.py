9a073688950dae45ce2a7c4897a8070eeaa79086
denisG99 <gasparollodenis@gmail.com>
2022-11-03 11:50:14 +0100

    ADD node type enumeration

---
+++ src/model/NodeType.py
@@ -0,0 +1,12 @@
+"""""
+ * This class is an enumeration that decribe the node type.
+ 
+ * The node types are: DECISION, LEAF or SPLIT.
+"""""
+
+from enum import Enum
+
+class NodeType(Enum):
+    DECISION = 0
+    LEAF = 1
+    SPLIT = 2
