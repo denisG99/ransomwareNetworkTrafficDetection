9585fcbce81620755bac831cc27d766f2ce964c0
denisG99 <gasparollodenis@gmail.com>
2022-11-08 18:06:56 +0100

    implementing Perceptron with sklearn

--- src/model/NeuralNetwork.py
+++ src/model/NeuralNetwork.py
@@ -1,40 +1,52 @@
 from dataclasses import dataclass, field
 
-from Perceptron import Perceptron
+#from Perceptron import Perceptron
+from sklearn.linear_model import Perceptron
 
 import numpy as np
 
 @dataclass
 class NeuralNetwork:
     __perceptrons : np.ndarray = field(init=False)
-    __outputs : np.ndarray = field(init=False)
+    #__outputs : np.ndarray = field(init=False)
     __num_input_features: int
     __num_classi : int = 2
+    __num_perceptrons: int = 1
 
     def __post_init__(self):
-        self.__outputs = np.zeros(self.__num_classi)
-        self.__perceptrons = np.empty(self.__num_classi, dtype=Perceptron)
+        #if self.__num_classi == 2:
+         #   self.__num_perceptrons = 1
+        #else:
+         #   self.__num_perceptrons = self.__num_classi
 
-        for i in range(self.__num_classi):
-            self.__perceptrons[i] = Perceptron(self.__num_input_features)
+        self.__perceptrons = np.empty(self.__num_perceptrons, dtype=Perceptron)
 
-    def fit(self, X: np.ndarray, y: np.ndarray, epochs: int) -> None:
+        for i in range(self.__num_perceptrons):
+            self.__perceptrons[i] = Perceptron(verbose=1, eta0=.1)
+
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         for perceptron in self.__perceptrons:
-            perceptron.fit(X, y, epochs)
+            perceptron.fit(X, y)
+            #print(f'\tMean Accuracy: {results.best_score_}')
+            #print(f'\tConfig: {results.best_params_}')
 
             #print('\nModel parameters:')
             #print(f'\tWeights: {perceptron.get_weigths()}')
             #print(f'\tBias: {perceptron.get_bias()}\n')
 
-    def evaluate(self, X: np.ndarray, y: np.ndarray) -> None:
+    def predict(self, X) -> float:
+        for perceptron in self.__perceptrons:
+                output = perceptron.predict(X)
+
+        return output
+
+    def evaluate(self, X: np.ndarray, y: np.ndarray, str: str) -> None:
         i: int = 0
 
         for perceptron in self.__perceptrons:
-            print(f"\tPerceptron {i}")
+            print(f"Perceptron {i + 1}")
             i += 1
-            print(f'\t\t{perceptron.evaluate(X, y)}')
-
-
+            print(f'\t{str}: {perceptron.score(X, y) * 100}')
 #-----------------------------------------------------------------------------------------------------------------------
 
 def main() -> None:
@@ -59,13 +71,19 @@ def main() -> None:
 
     nn = NeuralNetwork(2)
 
-    nn.fit(X_train, y_train, 5)
+    nn.fit(X_train, y_train)
 
-    print("Train accuracy:")
-    nn.evaluate(X_train, y_train)
+    nn.evaluate(X_train, y_train, "Training accuracy")
+    nn.evaluate(X_test, y_test, "Testing accuracy")
 
-    print("Test accuracy:")
-    nn.evaluate(X_test, y_test)
+    #for pattern, expected_y in zip(X_test, y_test):
+     #   print(f"{nn.predict(pattern.reshape(1, -1))} -> {expected_y}")
+
+    #print("Train accuracy:")
+    #nn.evaluate(X_train, y_train)
+
+    #print("Test accuracy:")
+    #nn.evaluate(X_test, y_test)
 
 
 if __name__ == "__main__":
--- src/model/Node.py
+++ src/model/Node.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from typing import Tuple
 
 from NeuralNetwork import NeuralNetwork
 from NodeType import NodeType
@@ -10,8 +11,6 @@ from sklearn.model_selection import train_test_split
 #from numpy.random import RandomState
 import numpy as np
 
-import pandas as pd
-
 @dataclass
 class Node:
     #fields
@@ -20,7 +19,7 @@ class Node:
     __num_features: int #number of features into dataset
     __label: Classification = Classification.NONE
     __type : NodeType = NodeType.DECISION
-    __child : list = field(init=False, default_factory=list) #list containing childs nodes
+    __childs : list = field(init=False, default_factory=list) #list containing childs nodes
     __depth : int = 0 #TODO: forse da spostare
     __threshold : float = 0.0
     __toler : float = 0.0 #tollerance that indicate the end of the branch training. Will be use in convergence test #TODO:forse da spostare
@@ -34,23 +33,28 @@ class Node:
         return self.__num_features
 
     #TODO: da finire
-    def train(self, epochs: int = 1) -> None:
+    def train(self) -> None:
         data = self.__patterns[:, 0 : self.__num_features]  # patterns dataset
         target = self.__patterns[:, self.__num_features]  # labels dataset
         #TODO: rendere suddivisione riproducibile
         X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=.3,train_size=.7)  # dataset split with 70-30 splitting rule
 
-        self.__nn.fit(X_train, y_train, epochs)
+        self.__nn.fit(X_train, y_train)
+        self.__nn.evaluate(X_train, y_train, "Train accuracy")
+        self.__nn.evaluate(X_test, y_test, "Test accuracy")
 
-        #TODO: splitting dataset based on prediction to create LTS
+        goodware_lts, malware_lts = self.__dataset_split(self.__patterns, data)
+
+        #TODO: realizzare creazione nodo tenendo conto delle caratteristiche degli LTS
 
 
-        print(f'Train accuracy:')
-        self.__nn.evaluate(X_train, y_train)
+        np.append(self.__childs, [Node(malware_lts, self.__num_features), Node(goodware_lts, self.__num_features)])
 
-        print(f'Test accuracy:')
-        self.__nn.evaluate(X_test, y_test)
+        for child in self.__childs:
+            if not self.__type == NodeType.LEAF:
+                child.train()
 
+    """"
     def split_node(self) -> None:
         self.__type = NodeType.SPLIT
         left_patterns, right_patterns = np.zeros(1), np.zeros(1) #TODO: da fare funzione che dividi a metà il dataset
@@ -62,18 +66,60 @@ class Node:
 
         self.__child.append(left_node)
         self.__child.append(right_node)
+    """
 
-    def predict(self, X: np.ndarray) -> int:
-        pass
+    def __dataset_split(self, lts: np.ndarray, X: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        This function create a local datasets starting from starting dataset stored in current node based on decision boundary
+
+        IDEA:
+          foreach patterns into dataset will be compute the prediction and based on that the dataset will split
+
+        --------------
+
+        :param lts: Local Training Set that will be split
+        :param X: patterns whitch make predictions
+
+        :return: return an array containig the two dataset split
+        """
+        lts0 = np.array({}, dtype=np.ndarray)
+        lts1 = np.array({}, dtype=np.ndarray)
+
+        for pattern, row in zip(X, lts):
+            if self.__nn.predict(pattern) == 1.0:
+                np.append(lts1, row)
+            else:
+                np.append(lts0, row)
+
+        return lts0, lts1
+
+    def predict(self, X) -> float:
+        return self.__nn.predict(X)
 
 
 #-----------------------------------------------------------------------------------------------------------------------
 
 def main() -> None:
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+
     patterns = pd.read_csv("toydata.csv").to_numpy()
     root = Node(patterns, patterns.shape[1] - 1)
+    X, y = patterns[:, :2], patterns[:, 2]
 
-    root.train(5)
+    #plotting
+    plt.scatter(X[y == 0, 0], X[y == 0, 1], label='class 0', marker='o')
+    plt.scatter(X[y == 1, 0], X[y == 1, 1], label='class 1', marker='s')
+    plt.title('Dataset')
+    plt.xlabel('feature 1')
+    plt.ylabel('feature 2')
+    plt.xlim([-3, 3])
+    plt.ylim([-3, 3])
+    plt.legend()
+    plt.show()
+
+    root.train()
 
 if __name__ == "__main__":
     main()
