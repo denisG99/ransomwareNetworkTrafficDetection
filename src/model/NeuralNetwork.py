4791a6e02a44d809090eda3e619ca032136e803a
denisG99 <gasparollodenis@gmail.com>
2022-11-23 12:16:32 +0100

    implementing NN using keras

--- src/model/NeuralNetwork.py
+++ src/model/NeuralNetwork.py
@@ -1,17 +1,31 @@
 from dataclasses import dataclass, field
 
+from keras.callbacks import EarlyStopping, CSVLogger
 #from Perceptron import Perceptron
-from sklearn.linear_model import Perceptron
+#from sklearn.linear_model import Perceptron
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.activations import hard_sigmoid
 
 import numpy as np
 
 @dataclass
 class NeuralNetwork:
-    __perceptrons : np.ndarray = field(init=False)
+    """
+    Costruttore:
+        NeuralNetwork(__num_input_features,
+                      __num_classi = 2,
+                      __num_perceptrons = 1)
+    """
+    #__perceptrons : np.ndarray = field(init=False)
     #__outputs : np.ndarray = field(init=False)
+    __model : Sequential = field(init=False)
     __num_input_features: int
     __num_classi : int = 2
     __num_perceptrons: int = 1
+
+    def set_num_classi(self, num_classi):
+        self.__num_classi = num_classi
 
     def __post_init__(self):
         #if self.__num_classi == 2:
@@ -19,14 +33,23 @@ class NeuralNetwork:
         #else:
          #   self.__num_perceptrons = self.__num_classi
 
-        self.__perceptrons = np.empty(self.__num_perceptrons, dtype=Perceptron)
+        #self.__perceptrons = np.empty(self.__num_perceptrons, dtype=Perceptron)
+        self.__model = Sequential()
 
-        for i in range(self.__num_perceptrons):
-            self.__perceptrons[i] = Perceptron(verbose=1, eta0=.1, early_stopping=True, n_iter_no_change=5, tol=1e-4)
+        self.__model.add(Dense(self.__num_perceptrons, input_shape=(self.__num_input_features,), activation=hard_sigmoid, kernel_initializer='glorot_uniform'))
+        self.__model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) #TODO: ampliare metriche
 
-    def fit(self, X: np.ndarray, y: np.ndarray) -> object:
-        for perceptron in self.__perceptrons:
-            model = perceptron.fit(X, y)
+        #for i in range(self.__num_perceptrons):
+         #   self.__perceptrons[i] = Perceptron(verbose=1, eta0=.1, early_stopping=True, n_iter_no_change=5, tol=1e-4) #TODO: mettere learning rate personalizzabile (eta0; )
+
+    def fit(self, X: np.ndarray, y: np.ndarray, epochs: int, wait_epochs: int):
+        callbacks = [EarlyStopping(monitor="val_loss", patience=wait_epochs, verbose=1)]
+        #callbacks = [EarlyStopping(monitor="val_loss", patience=wait_epochs, verbose=1),
+         #            CSVLogger("log.csv", separator=',', append=False)]
+
+        hystory = self.__model.fit(X, y, epochs=epochs, batch_size=1, verbose=1, validation_split=0.1, callbacks=callbacks)
+        #for perceptron in self.__perceptrons:
+         #   model = perceptron.fit(X, y)
 
             #print(model.coef_)
             #print(model.intercept_)
@@ -39,21 +62,27 @@ class NeuralNetwork:
             #print('\nModel parameters:')
             #print(f'\tWeights: {perceptron.get_weigths()}')
             #print(f'\tBias: {perceptron.get_bias()}\n')
-            return model
+            #print(f'\tN_iter: {model.n_iter_}')
+            #print(f'\tN_weigth update: {model.t_}')
+        return hystory
 
-    def predict(self, X) -> float:
-        for perceptron in self.__perceptrons:
-            output = perceptron.predict(X)
+    def predict(self, X: np.ndarray) -> float:
+        prediction = self.__model.predict(X, batch_size=X.shape[1], verbose=0)
+        #for perceptron in self.__perceptrons:
+         #   output = perceptron.predict(X)
 
-        return output
+        return prediction
 
-    def evaluate(self, X: np.ndarray, y: np.ndarray, str: str) -> None:
+    def evaluate(self, X: np.ndarray, y: np.ndarray) -> dict:
+        """
         i: int = 0
 
         for perceptron in self.__perceptrons:
             print(f"Perceptron {i + 1}")
             i += 1
             print(f'\t{str}: {perceptron.score(X, y) * 100}')
+        """
+        return self.__model.evaluate(X, y, return_dict=True)
 #-----------------------------------------------------------------------------------------------------------------------
 
 def main() -> None:
@@ -68,20 +97,21 @@ def main() -> None:
     shuffle_rng.shuffle(shuffle_idx)
     X, y = X[shuffle_idx], y[shuffle_idx]
 
-    X_train, X_test = X[shuffle_idx[:70]], X[shuffle_idx[70:]]
-    y_train, y_test = y[shuffle_idx[:70]], y[shuffle_idx[70:]]
+    X_train, X_test = X[shuffle_idx[:80]], X[shuffle_idx[80:]]
+    y_train, y_test = y[shuffle_idx[:80]], y[shuffle_idx[80:]]
 
     # Normalize (mean zero, unit variance)
-    mu, sigma = X_train.mean(axis=0), X_train.std(axis=0)
-    X_train = (X_train - mu) / sigma
-    X_test = (X_test - mu) / sigma
+    #mu, sigma = X_train.mean(axis=0), X_train.std(axis=0)
+    #X_train = (X_train - mu) / sigma
+    #X_test = (X_test - mu) / sigma
 
     nn = NeuralNetwork(2)
 
-    model = nn.fit(X_train, y_train)
+    statistics = nn.fit(X_train, y_train, 500, 5)
 
-    nn.evaluate(X_train, y_train, "Training accuracy")
-    nn.evaluate(X_test, y_test, "Testing accuracy")
+    print(f"Training -> {nn.evaluate(X_train, y_train)}")
+    print(f"Testing -> {nn.evaluate(X_test, y_test)}")
+    print(statistics.history['loss'])
 
     #for pattern, expected_y in zip(X_test, y_test):
      #   print(f"{nn.predict(pattern.reshape(1, -1))} -> {expected_y}")
