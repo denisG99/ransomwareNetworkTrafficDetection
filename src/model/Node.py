dca2c1302e92d84fd358c220b03ba4cd853d9769
denisG99 <gasparollodenis@gmail.com>
2022-12-07 17:49:53 +0100

    Implementing prediction function of Node

--- src/model/Node.py
+++ src/model/Node.py
@@ -26,11 +26,11 @@ class Node:
              __threshold = 0.0,
              __num_classi = 2)
     """
-    __nn : NeuralNetwork = field(init=False) #perceptron definition
+    __nn : NeuralNetwork = field(init=False, repr=False) #perceptron definition
     __entropy: float = field(init=False)  # dataset entropy
     __childs: np.ndarray = field(init=False)  # list containing childs nodes
     __is_splitted: bool = field(init=False)  # indicate if node is split
-    __is_removed: bool = field(init=False)  # indicate if node has executed pattern removal
+    #__is_removed: bool = field(init=False)  # indicate if node has executed pattern removal
     __patterns: np.ndarray  # field containing training data for perceptron
     __num_features: int  # number of features into dataset
     __label: Classification = Classification.NONE
@@ -56,7 +56,7 @@ class Node:
         self.__entropy, occurs = self.__compute_entropy()
         self.__childs = np.array([], dtype=Node)
         self.__is_splitted = False
-        self.__is_removed = False
+        #self.__is_removed = False
 
         if self.__entropy <= ENTROPY_TH:
             self.__type = NodeType.LEAF
@@ -127,11 +127,11 @@ class Node:
             #print(goodware_lts)
             #print(malware_lts)
 
-            #for child in self.__childs:
-             #   child.train(epochs, wait_epochs, plot)
+            for child in self.__childs:
+                child.train(epochs, wait_epochs, plot)
 
-            print(f"Training -> {self.__nn.evaluate(X_train, y_train)}")
-            print(f"Testing -> {self.__nn.evaluate(X_test, y_test)}")
+            #print(f"Training -> {self.__nn.evaluate(X_train, y_train)}")
+            #print(f"Testing -> {self.__nn.evaluate(X_test, y_test)}")
 
             return plot
 
@@ -300,7 +300,7 @@ class Node:
         for pattern, row in zip(X, lts):
             #print(pattern)
             #print(row)
-            if self.predict(pattern) == 1.0:
+            if self.__nn.predict(pattern.reshape(1, -1)) >= 0.5:
                 lts1 = np.append(lts1, row)
             else:
                 lts0 = np.append(lts0, row)
@@ -316,10 +316,14 @@ class Node:
         #return lts0.reshape(row0, self.__num_features + 1), lts1.reshape(row1, self.__num_features + 1)
         return lts0, lts1
 
-    def predict(self, X) -> float:
-        return self.__nn.predict(X.reshape(1, -1))
-
-
+    def predict(self, X) -> Classification:
+        if self.__type == NodeType.LEAF:
+            return self.__label
+        else:
+            if self.__nn.predict(X.reshape(1, -1)) >= 0.5:
+                return self.__childs[0].predict(X)
+            else:
+                return self.__childs[1].predict(X)
 #-----------------------------------------------------------------------------------------------------------------------
 
 def main() -> None:
@@ -330,13 +334,13 @@ def main() -> None:
     import numpy as np
 
 
-    patterns = pd.read_csv("toydata.csv", header=None).to_numpy()
+    #patterns = pd.read_csv("toydata.csv", header=None).to_numpy()
     #print(patterns.shape)
-    #X, y = make_circles(100)
+    X, y = make_circles(100)
 
     #print(X)
-    #y= np.reshape(y, (100, 1))
-    #patterns = np.append(X, y, axis=1)
+    y= np.reshape(y, (100, 1))
+    patterns = np.append(X, y, axis=1)
 
     root = Node(patterns, patterns.shape[1] - 1)
     X, y = patterns[:, :2], patterns[:, 2]
@@ -366,6 +370,8 @@ def main() -> None:
     plt.legend()
     plt.show()
 
+    #print(root)
+    print(root.predict(np.array([0, 0,]).reshape(1, -1)))
     """
     lts0, lts1 = root.dataset_split(patterns, X)
     #root.dataset_split(patterns, X)
