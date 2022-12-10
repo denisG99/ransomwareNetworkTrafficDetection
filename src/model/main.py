166d4915587c5a28e8e63566efd03ec3ac23e636
denisG99 <gasparollodenis@gmail.com>
2022-12-10 11:21:15 +0100

    ADD main.py

---
+++ src/model/main.py
@@ -0,0 +1,23 @@
+from NeuralTree import NeuralTree
+
+import numpy as np
+import time
+
+def main() -> None:
+    start_time = time.time()
+    nt = NeuralTree("/Users/denisgasparollo/PycharmProjects/ransomwareNetworkTrafficDetection/src/model/toydata.csv", 5)
+
+    nt.train(250)
+
+    print(f"Time for training: {time.time() - start_time} secondi")
+    print(nt.make_prediction(np.array([0, 0])))
+
+    nt.save_model()
+
+    nt = nt.load_model("/Users/denisgasparollo/PycharmProjects/ransomwareNetworkTrafficDetection/src/model.pkl")
+
+    print(nt.make_prediction(np.array([0, 0])))
+
+
+if __name__ == "__main__":
+    main()
