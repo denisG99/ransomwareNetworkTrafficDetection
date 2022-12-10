bf787763c45929ce62b7f3223fc56c46bd7b4ae3
denisG99 <gasparollodenis@gmail.com>
2022-12-10 17:07:37 +0100

    Neural Tree evaluation (precision)

--- src/model/main.py
+++ src/model/main.py
@@ -1,3 +1,7 @@
+"""
+This file contain the script file for testing Neural Tree
+"""
+
 from NeuralTree import NeuralTree
 
 import numpy as np
@@ -7,16 +11,16 @@ def main() -> None:
     start_time = time.time()
     nt = NeuralTree("/Users/denisgasparollo/PycharmProjects/ransomwareNetworkTrafficDetection/src/model/toydata.csv", 5)
 
-    nt.train(250)
+    nt.train(250, verbose=0)
 
     print(f"Time for training: {time.time() - start_time} secondi")
-    print(nt.make_prediction(np.array([0, 0])))
+    print(nt.make_predictions(np.array([0, 0])))
 
-    nt.save_model()
+    #nt.save_model()
 
-    nt = nt.load_model("/Users/denisgasparollo/PycharmProjects/ransomwareNetworkTrafficDetection/src/model.pkl")
+    #nt = nt.load_model("/Users/denisgasparollo/PycharmProjects/ransomwareNetworkTrafficDetection/src/model.pkl")
 
-    print(nt.make_prediction(np.array([0, 0])))
+    #print(nt.make_prediction(np.array([0, 0])))
 
 
 if __name__ == "__main__":
